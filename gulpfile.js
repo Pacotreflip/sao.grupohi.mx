8a90fe63612ef3a0cc1f0a8cbbb5dfdf5ab1b334
JFEsquivel <jfesquivel@grupohi.mx>
2017-06-09 14:56:48 -0500

    MÓDULO CONTABLE: (Gulp) Crear Póliza Tipo Completo

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,9 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    /*mix.less('app.less');
+    mix.less('app.less');
     mix.less('admin-lte/AdminLTE.less');
     mix.less('bootstrap/bootstrap.less');
-    */
     mix.browserify('app.js');
     mix.version(['public/css/app.css', 'public/js/app.js']);
 });
