95ea0db780fec8596590b6fb9924bd072f8c7153
AORO <antonio_oro18@hotmail.com>
2017-06-15 11:37:30 -0500

    detalle vigencia poliza

--- resources/views/modulo_contable/poliza_tipo/show.blade.php
+++ resources/views/modulo_contable/poliza_tipo/show.blade.php
@@ -10,26 +10,36 @@
 
                 <div class="box box-solid">
                     <div class="box-header with-border">
-                        <h3 class="box-title">{{ $poliza_tipo->transaccion }}</h3>
+                        <h3 class="box-title">{{ $poliza_tipo->transaccion }} &nbsp;  @if($poliza_tipo->vigente)
+                                <span class="label label-success">Vigente</span>
+                            @else
+                                <span class="label label-danger">No Vigente</span>
+                            @endif</h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                        <dl>
-                            <dt>ID</dt>
-                            <dd>{{$poliza_tipo->id}}</dd>
-                            <dt>USUARIO QUE REGISTRÓ</dt>
-                            <dd>{{$poliza_tipo->userRegistro}}</dd>
-                            <dt>FECHA Y HORA DE REGISTRO</dt>
-                            <dd>{{$poliza_tipo->created_at->format('Y-m-d h:i:s a')}}</dd>
-                            <dt>VIGENCIA</dt>
-                            <dd>
-                                @if($poliza_tipo->vigente)
-                                    <span class="label label-success">Vigente</span>
-                                @else
-                                    <span class="label label-danger">No Vigente</span>
-                                @endif
-                            </dd>
-                        </dl>
+
+                        <div class="col-sm-6">
+                            <dl>
+                                <dt>ID</dt>
+                                <dd>{{$poliza_tipo->id}}</dd>
+                                <dt>USUARIO QUE REGISTRÓ</dt>
+                                <dd>{{$poliza_tipo->userRegistro}}</dd>
+                                <dt>FECHA Y HORA DE REGISTRO</dt>
+                                <dd>{{$poliza_tipo->created_at->format('Y-m-d h:i:s a')}}</dd>
+                                <dt>VIGENCIA</dt>
+                            </dl>
+                        </div>
+                        <div class="col-sm-6">
+                            <dl>
+                                <dt>INICIO DE VIGENCIA</dt>
+                                <dd>{{$poliza_tipo->inicio_vigencia->format('Y-m-d')}}</dd>
+                                <dt>FIN DE VIGENCIA</dt>
+                                <dd>@if($poliza_tipo->fin_vigencia){{$poliza_tipo->fin_vigencia->format('Y-m-d')}}@else
+                                        N/A @endif</dd>
+                            </dl>
+                        </div>
+
                     </div>
                     <!-- /.box-body -->
                 </div>
@@ -38,40 +48,42 @@
     </div>
 
     @if($poliza_tipo->movimientos)
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-success">
-                <div class="box-header">
-                    <h3 class="box-title">Movimientos</h3>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-success">
+                    <div class="box-header">
+                        <h3 class="box-title">Movimientos</h3>
 
-                    <div class="col-sm-12">
-                        <div class="row">
-                            <div class="box-body">
-                                <table class="table table-bordered table-striped ">
-                                    <thead>
-                                    <tr>
-                                        <th>Cuenta Contable</th>
-                                        <th>Cargo</th>
-                                        <th>Abono</th>
+                        <div class="col-sm-12">
+                            <div class="row">
+                                <div class="box-body">
+                                    <table class="table table-bordered table-striped ">
+                                        <thead>
+                                        <tr>
+                                            <th>Cuenta Contable</th>
+                                            <th>Cargo</th>
+                                            <th>Abono</th>
 
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($poliza_tipo->movimientos as $movimieto)
-                                    <tr>
-                                        <td>{{$movimieto->cuentaContable->tipoCuentaContable}}</td>
-                                        <td>@if($movimieto->tipoMovimiento->id==1)<span class="fa fa-check-circle"></span>@endif</td>
-                                        <td>@if($movimieto->tipoMovimiento->id==2)<span class="fa fa-check-circle"></span>@endif</td>
-                                    </tr>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($poliza_tipo->movimientos as $movimieto)
+                                            <tr>
+                                                <td>{{$movimieto->cuentaContable->tipoCuentaContable}}</td>
+                                                <td>@if($movimieto->tipoMovimiento->id==1)<span
+                                                            class="fa fa-check-circle"></span>@endif</td>
+                                                <td>@if($movimieto->tipoMovimiento->id==2)<span
+                                                            class="fa fa-check-circle"></span>@endif</td>
+                                            </tr>
                                         @endforeach
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     @endif
 @endsection
