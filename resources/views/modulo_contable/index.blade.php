6f33f2e502b23a1de77b013b6c1966916fed0137
JFEsquivel <jfesquivel@grupohi.mx>
2017-06-15 19:04:40 -0500

    Quitar dd del index de modulo contable

--- resources/views/modulo_contable/index.blade.php
+++ resources/views/modulo_contable/index.blade.php
@@ -1,1 +1,6 @@
-{{dd($currentObra)}}
+@extends('modulo_contable.layout')
+@section('title', 'Módulo Contable')
+@section('contentheader_title', 'MÓDULO CONTABLE')
+@section('main-content')
+    {!! Breadcrumbs::render('modulo_contable.index') !!}
+@endsection
