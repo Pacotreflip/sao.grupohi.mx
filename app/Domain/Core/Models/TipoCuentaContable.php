3a2e34b523f2367f79e81c9f7271f868531e0127
JLopezA <gsus.arriaga@gmail.com>
2017-06-19 12:03:37 -0500

    SoftDeletes de TipoCuentaContable

--- app/Domain/Core/Models/TipoCuentaContable.php
+++ app/Domain/Core/Models/TipoCuentaContable.php
@@ -4,9 +4,13 @@ namespace Ghi\Domain\Core\Models;
 
 use Ghi\Domain\Core\Models\Scopes\ObraScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TipoCuentaContable extends Model
 {
+
+    use SoftDeletes;
+
     protected $connection = 'cadeco';
     protected $table = 'Contabilidad.int_tipos_cuentas_contables';
     protected $primaryKey = 'id_tipo_cuenta_contable';
@@ -17,6 +21,8 @@ class TipoCuentaContable extends Model
         'id_obra',
         'motivo'
     ];
+
+    protected $dates = ['deleted_at'];
 
     protected static function boot()
     {
