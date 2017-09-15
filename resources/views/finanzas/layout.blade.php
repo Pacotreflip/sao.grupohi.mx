524fb46c493551ed91377691a4b4d75213376553
AORO <antonio_oro18@hotmail.com>
2017-09-15 12:43:54 -0500

    permission menu

--- resources/views/finanzas/layout.blade.php
+++ resources/views/finanzas/layout.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('content-menu')
     <ul class="sidebar-menu">
+
+        @permission(['consultar_comprobante_fondo_fijo'])
         <li class="treeview">
             <a href="#">
                 <i class="fa fa-circle-o"></i>
@@ -10,10 +12,11 @@
                 </span>
             </a>
             <ul class="treeview-menu">
+                @permission('consultar_comprobante_fondo_fijo')
                 <li><a href="{{route('finanzas.comprobante_fondo_fijo.index')}}"><i class='fa  fa-circle-o'></i> <span>Comprobantes de Fondo Fijo</span></a></li>
-
+                @endpermission
             </ul>
         </li>
-
+        @endpermission
     </ul>
 @endsection
