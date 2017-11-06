6ca8acc8c82114f060456f60987f988ea6a58280
Francisco Esquivel <froker_ad32@hotmail.com>
2017-11-06 11:57:56 -0600

    Avance de API DOCS

--- app/Api/Controllers/EmpresaController.php
+++ app/Api/Controllers/EmpresaController.php
@@ -26,6 +26,37 @@ class EmpresaController extends Controller
         $this->empresa = $empresa;
     }
 
+    /**
+     * @api {post} /empresa Registrar Empresa
+     * @apiVersion 1.0.0
+     * @apiGroup Empresa
+     * @apiParam {String} rfc RFC de la Empresa
+     * @apiParam {String} razon_social Razón Social de la Empresa
+     *
+     * @apiError StoreResourceFailedException Error al registrar una Empresa
+     * @apiErrorExample Error-Response:
+     *   HTTP/1.1 422 Unprocessable Entity
+     *   {
+     *     "message": "error descropton",
+     *     "errors": {
+     *       "param": ["error 1", "error 2"]
+     *       ...
+     *     },
+     *     "status_code": 422
+     *   }
+     *
+     * @apiSuccess (200) {Object} data Datos de la Empresa Registrada
+     * @apiSuccessExample Success-Response
+     *   HTTP/1.1 200 OK
+     *   {
+     *     "data": {
+     *       "id_empresa": "xxx",
+     *       "tipo_empresa": "x",
+     *       "razon_social": "Razón Social",
+     *       "rfc": "000000XXX"
+     *     }
+     *   }
+     */
     public function store(Request $request)
     {
         $empresa = $this->empresa->create($request->all());
--- app/Domain/Core/Transformers/EmpresaTransformer.php
+++ app/Domain/Core/Transformers/EmpresaTransformer.php
@@ -14,6 +14,12 @@ use League\Fractal\TransformerAbstract;
 class EmpresaTransformer extends TransformerAbstract {
 
     public function transform(Empresa $empresa) {
-        return $empresa->toArray();
+
+        return [
+            "id_empresa" => $empresa->id_empresa,
+            "tipo_empresa" => $empresa->tipo_empresa,
+            "razon_social" => $empresa->razon_social,
+            "rfc" => $empresa->rfc
+        ];
     }
 }
