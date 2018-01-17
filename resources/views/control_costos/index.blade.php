491f28fc18133392855a3c356d40a54a15818aa2
jfesquivel <jfesquivel@grupohi.mx>
2018-01-17 11:14:35 -0600

    Corrección de permisos en controller de solicitud de reclasificación

--- resources/views/control_costos/index.blade.php
+++ resources/views/control_costos/index.blade.php
@@ -6,17 +6,19 @@
 @endsection
 @section('main-content')
 
+    @permission(['consultar_reclasificacion'])
     <div class="row">
         <div class="col-md-12">
             <div class="nav-tabs-custom">
                 <div class="tab-content">
                     <div class="tab-pane active" id="subcontratos">
                         <ul class="nav nav-stacked">
-                            @permission(['consultar_reclasificacion', 'solicitar_reclasificacion'])
+                            @permission('consultar_reclasificacion')
                             <li><a href="{{ route('control_costos.solicitar_reclasificacion.index') }}">SOLICITAR RECLASIFICACIÓN</a></li>
-                            @endpermission
-                            @permission(['consultar_reclasificacion', 'autorizar_reclasificacion'])
                             <li ><a href="{{route('control_costos.solicitudes_reclasificacion.index')}}">SOLICITUDES DE RECLASIFICACIÓN REGISTRADAS</a></li>
+                            @endpermission
+                            @permission('eliminar')
+                            <li><a></a></li>
                             @endpermission
                         </ul>
                     </div>
@@ -24,6 +26,7 @@
             </div>
         </div>
     </div>
+    @endpermission
 
 @endsection
 
