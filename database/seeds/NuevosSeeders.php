e8c00ce90752d3d41193d87819a15128ae543447
AORO <antonio_oro18@hotmail.com>
2018-01-24 19:37:24 -0600

    seeder llenar operacion

---
+++ database/seeds/NuevosSeeders.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class NuevosSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Flujo Operativo solicitud cambios al presupuesto
+        $this->call(ControlPresupuestoFlujoOperativoSeeder::class);
+        //Flujo Operativo solicitud cambios al presupuesto
+        $this->call(ControlPresupuestoEstatusSeeder::class);
+    }
+}
