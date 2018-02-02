daedebfdcbf89fb79ca8773f1337372b0bd072e9
jfesquivel <jfesquivel@grupohi.mx>
2018-02-01 19:53:53 -0600

    avance 2

--- resources/views/control_presupuesto/cambio_presupuesto/escalatoria.blade.php
+++ resources/views/control_presupuesto/cambio_presupuesto/escalatoria.blade.php
@@ -10,19 +10,9 @@
                         <div class="box-body">
                             <div class="row">
                                 <div class="col-md-6">
-                                    <div class="form-group" :class="{'has-error': validation_errors.has('form_add_partida.Descripción')}">
-                                        <label><b>Descripción</b></label>
-                                        <input v-validate="'required'" :name="'Descripción'" type="text" class="form-control input-sm" v-model="escalatoria.descripcion">
-                                        <label class="help" v-show="validation_errors.has('form_add_partida.Descripción')">@{{ validation_errors.first('form_add_partida.Descripción') }}</label>
-                                    </div>
+
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group" :class="{'has-error': validation_errors.has('form_add_partida.Importe')}">
-                                        <label><b>Importe</b></label>
-                                        <input v-validate="'required'" :name="'Importe'" type="number" step="any" class="form-control input-sm" v-model="escalatoria.importe">
-                                        <label class="help" v-show="validation_errors.has('form_add_partida.Importe')">@{{ validation_errors.first('form_add_partida.Importe') }}</label>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                         <div class="box-footer">
@@ -61,7 +51,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr v-for="(i, partida) in form.partidas">
+                                <tr v-for="(partida, i) in form.partidas">
                                     <td>@{{ i + 1 }}</td>
                                     <td>@{{ partida.descripcion }}</td>
                                     <td>@{{ partida.importe }}</td>
@@ -76,12 +66,55 @@
                                 <span v-if="guardando">
                                     <i class="fa fa-spin fa-spinner"></i> Guardando
                                 </span>
-                                <v-else>
+                                <span v-else>
                                     <i class="fa fa-save"></i> Guardar
-                                </v-else>
+                                </span>
                             </button>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal add partida -->
+        <div id="partidas_modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="PartidasModal">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Agregar Partida a la Solicitud</h4>
+                    </div>
+                    <form id="form_add_partida" @submit.prevent="validateForm('form_add_partida', 'add_partida')"  data-vv-scope="form_add_partida">
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group" :class="{'has-error': validation_errors.has('form_add_partida.Descripción')}">
+                                        <label><b>Descripción</b></label>
+                                        <input v-validate="'required'" :name="'Descripción'" type="text" class="form-control input-sm" v-model="escalatoria.descripcion">
+                                        <label class="help" v-show="validation_errors.has('form_add_partida.Descripción')">@{{ validation_errors.first('form_add_partida.Descripción') }}</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group" :class="{'has-error': validation_errors.has('form_add_partida.Importe')}">
+                                        <label><b>Importe</b></label>
+                                        <input v-validate="'required'" :name="'Importe'" type="number" step="any" class="form-control input-sm" v-model="escalatoria.importe">
+                                        <label class="help" v-show="validation_errors.has('form_add_partida.Importe')">@{{ validation_errors.first('form_add_partida.Importe') }}</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal" @click="validation_errors.clear('form_add_partida')">Cerrar</button>
+                            <button type="submit" class="btn btn-primary" :disabled="cargando">
+                            <span v-if="cargando">
+                                <i class="fa fa-spinner fa-spin"></i> Guardando
+                            </span>
+                                <span v-else>
+                                <i class="fa fa-save"></i> Guardar
+                            </span>
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
