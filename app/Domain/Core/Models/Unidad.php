0dbad6ebe558284599e069bc60c3101857f9d52d
AORO <antonio_oro18@hotmail.com>
2018-02-15 21:22:53 -0600

    unidad refactor

---
+++ app/Domain/Core/Models/Unidad.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francisco Esquivel
+ * Date: 26/09/2017
+ * Time: 11:38 AM
+ */
+
+namespace Ghi\Domain\Core\Models;
+
+
+class Unidad extends \Ghi\Core\Models\Unidad
+{
+    /**
+     * @var bool
+     */
+    public $incrementing=false;
+}
