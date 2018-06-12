16717b3520d243a361ae087ca800b5c9c36085a5
julio sanchez <julio.sanchez.igh@hotmail.com>
2018-06-12 15:59:19 -0500

    correciones

--- app/Domain/Core/Layouts/Compras/AsignacionCargaProveedoresLayout.php
+++ app/Domain/Core/Layouts/Compras/AsignacionCargaProveedoresLayout.php
@@ -651,7 +651,6 @@ class AsignacionCargaProveedoresLayout extends ValidacionLayout
      */
     public function procesarDatos(array $cotizaciones)
     {
-        dd($cotizaciones);
         try {
             DB::connection('cadeco')->beginTransaction();
             DB::connection('controlrec')->beginTransaction();
