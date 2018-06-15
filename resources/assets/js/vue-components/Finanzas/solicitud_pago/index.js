7b83de3b6bf88cc0e1389825e6587755692f8694
jfesquivel <jfesquivel@grupohi.mx>
2018-06-15 13:03:59 -0500

    getSolicitudes en index

--- resources/assets/js/vue-components/Finanzas/solicitud_pago/index.js
+++ resources/assets/js/vue-components/Finanzas/solicitud_pago/index.js
@@ -3,7 +3,31 @@ Vue.component('solicitud-pago-index', {
         return {
             solicitudes: [],
             cargando: false
+        }
+    },
 
+    mounted: function () {
+        this.getSolicitudes().then(function (data) {
+            this.solicitudes = data;
+            this.cargando = false;
+        });
+    },
+
+    methods: {
+        getSolicitudes: function () {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                $.ajax({
+                    url: App.host + '/api/finanzas/solicitud_pago',
+                    type: 'GET',
+                    beforeSend: function () {
+                        self.cargando = true;
+                    },
+                    success: function (response) {
+                        resolve(response);
+                    }
+                })
+            });
         }
     }
 });
