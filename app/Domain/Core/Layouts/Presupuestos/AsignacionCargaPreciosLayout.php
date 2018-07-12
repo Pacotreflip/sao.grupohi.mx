4212a454369bec44a96ee104f7dc4627bfcf62ce
mmendiola <mmendiola@grupohi.mx>
2018-07-12 00:05:42 -0500

    Calcular subtotal moneda conversión
    Signed-off-by: mmendiola <mmendiola@grupohi.mx>

--- app/Domain/Core/Layouts/Presupuestos/AsignacionCargaPreciosLayout.php
+++ app/Domain/Core/Layouts/Presupuestos/AsignacionCargaPreciosLayout.php
@@ -645,7 +645,7 @@ class AsignacionCargaPreciosLayout extends ValidacionLayout
     public function procesarDatos(array $contratos)
     {
         $contrato_proyectado = $this->contrato_proyectado->find($this->id_contrato_proyectado);
-        Log::debug($contratos);
+        //Log::debug($contratos);
         try {
             DB::connection('cadeco')->beginTransaction();
             $error = 0;
@@ -725,7 +725,7 @@ class AsignacionCargaPreciosLayout extends ValidacionLayout
                                             //"clave" =>  $arrayPresupuesto['clave'],
                                             //"descripcion" => $arrayPresupuesto['descripcion'],
                                         ];
-                                        Log::error($dataUpdatePresupuesto);
+                                        //Log::error($dataUpdatePresupuesto);
                                         $updatePartidas = $presupuesto->update($dataUpdatePresupuesto);
                                         if (!$updatePartidas) {
                                             $contratos[$key]['error'][] = "No se puede guardar el registro";
--- app/Domain/Core/Models/Transacciones/CotizacionContrato.php
+++ app/Domain/Core/Models/Transacciones/CotizacionContrato.php
@@ -91,11 +91,11 @@ class CotizacionContrato extends Transaccion
      */
     public function getTotalesPresupiestos()
     {
-        return DB::connection('cadeco')
+            return DB::connection('cadeco')
             ->table('dbo.transacciones')
             ->select(
-                DB::raw("sum(cast(( contratos.cantidad_original * presupuestos.precio_unitario ) * ((100-case when presupuestos.PorcentajeDescuento is null then 0 else presupuestos.PorcentajeDescuento end)/100)*((100- case when transacciones.PorcentajeDescuento is null then 0 else transacciones.PorcentajeDescuento end)/100) as float(2))) as monto"),
-                DB::raw("sum(cast(( contratos.cantidad_original * presupuestos.precio_unitario ) * ((100-case when presupuestos.PorcentajeDescuento is null then 0 else presupuestos.PorcentajeDescuento end)/100)*((100- case when transacciones.PorcentajeDescuento is null then 0 else transacciones.PorcentajeDescuento end)/100) as float(2)))*.16 as impuesto")
+                DB::raw("sum(cast(( contratos.cantidad_original * presupuestos.precio_unitario ) *((100- case when transacciones.PorcentajeDescuento is null then 0 else transacciones.PorcentajeDescuento end)/100) as float(2))) as monto"),
+                DB::raw("sum(cast(( contratos.cantidad_original * presupuestos.precio_unitario ) *((100- case when transacciones.PorcentajeDescuento is null then 0 else transacciones.PorcentajeDescuento end)/100) as float(2)))*.16 as impuesto")
             )
             ->join('presupuestos', 'transacciones.id_transaccion', '=', 'presupuestos.id_transaccion')
             ->join('contratos', 'contratos.id_concepto', '=', 'presupuestos.id_concepto')
